5ec709b1cef42d481e18199af244f3e4f65d802b
alexi-ae <alexi.acunaespino@gmail.com>
2024-12-06 10:00:43 -0500

    base project

---
+++ src/main/java/com/system/bank/devops/SystemBankDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.system.bank.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemBankDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemBankDevopsApplication.class, args);
+    }
+
+}
